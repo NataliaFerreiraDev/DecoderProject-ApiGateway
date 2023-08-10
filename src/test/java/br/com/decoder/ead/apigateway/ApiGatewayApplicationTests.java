cd7929bb09f8bc2498d59e10ce154e2c9ba7b2d4
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-08-10 07:47:39 -0300

    Iniciando o gateway

---
+++ src/test/java/br/com/decoder/ead/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.decoder.ead.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
